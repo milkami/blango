805678612200c46ebd2773f43af9e8da76e35cc4
Milka Miličević <milka.milicevic3@gmail.com>
2024-03-27 07:34:30 +0000

    Finish html frameworks

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from blog.models import Tag, Post
+from blog.models import Tag, Post, Comment
 
 class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {"slug": ("title",)}
