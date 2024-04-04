5d65c9b4bd73401736e3a6c2ac95fd1bf15ecc3b
Milka Miličević <milka.milicevic3@gmail.com>
2024-04-04 13:33:06 +0000

    Finish caching

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -6,6 +6,8 @@ from django.utils.safestring import mark_safe
 from django.utils.html import format_html
 register = template.Library()
 user_model = get_user_model()
+import logging
+logger = logging.getLogger(__name__)
 
 @register.filter
 def author_details(author, current_user=None):
@@ -53,6 +55,7 @@ def endcol():
 @register.inclusion_tag("blog/post-list.html")
 def recent_posts(post):
     posts = Post.objects.exclude(pk=post.pk)[:5]
+    logger.debug("Loaded %d recent posts for post %d", len(posts), post.pk)
     return {"title": "Recent Posts", "posts": posts}
 
 
