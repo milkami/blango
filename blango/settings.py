213779e173c321dc1a3001f8768f7ece57612b35
Milka Miličević <milka.milicevic3@gmail.com>
2024-06-12 11:16:16 +0000

    Finish browsable API

--- blango/settings.py
+++ blango/settings.py
@@ -66,7 +66,15 @@ class Dev(Configuration):
       'allauth.socialaccount.providers.google',
       'rest_framework',
       'rest_framework.authtoken',
+      'drf_yasg',
   ]
+
+  SWAGGER_SETTINGS = {
+        "SECURITY_DEFINITIONS": {
+            "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+            "Basic": {"type": "basic"},
+        }
+    }
 
   MIDDLEWARE = [
       'django.middleware.security.SecurityMiddleware',
