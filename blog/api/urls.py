03ff9ed15c4fb8f48965685ba0ad10e211477124
Milka Miličević <milka.milicevic3@gmail.com>
2024-06-27 13:46:13 +0000

    Finish filtering

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -44,6 +44,11 @@ urlpatterns += [
     ),
 
     path("", include(router.urls)),
+    path(
+        "posts/by-time/<str:period_name>/",
+        PostViewSet.as_view({"get": "list"}),
+        name="posts-by-time",
+    ),
 ]
 
 #urlpatterns = format_suffix_patterns(urlpatterns)
