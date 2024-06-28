58e8eb28dfec937ce5eb07c15a4d59337dd7d68f
Milka Miličević <milka.milicevic3@gmail.com>
2024-06-28 07:18:02 +0000

    Finish django-versatileimagefield

--- blango/settings.py
+++ blango/settings.py
@@ -69,6 +69,7 @@ class Dev(Configuration):
       'rest_framework.authtoken',
       'drf_yasg',
       'django_filters',
+      'versatileimagefield',
   ]
 
   SWAGGER_SETTINGS = {
@@ -253,6 +254,9 @@ class Dev(Configuration):
         "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
         "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
   }
+
+  MEDIA_ROOT = BASE_DIR / "media"
+  MEDIA_URL = "/media/"
 
 class Prod(Dev):
     DEBUG = False
