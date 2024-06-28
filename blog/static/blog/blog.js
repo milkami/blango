7fcc17295c5dc0f299b7c4bcf20e603c916a4b8d
Milka Miličević <milka.milicevic3@gmail.com>
2024-06-28 10:39:12 +0000

    Finish ReactJS

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -95,3 +95,42 @@ const p = lazyAdd(3, 4)
 p.then(resolvedCallback, rejectedCallback)
 
 lazyAdd("nan", "alsonan").then(resolvedCallback, rejectedCallback)
+
+// React
+class ClickButton extends React.Component {
+  state = {
+    wasClicked: false
+  }
+
+  handleClick () {
+    this.setState(
+      {wasClicked: true}
+    )
+  }
+
+  render () {
+    let buttonText
+
+    if (this.state.wasClicked)
+      buttonText = 'Clicked!'
+    else
+      buttonText = 'Click Me'
+
+    return React.createElement(
+      'button',
+      {
+        className: 'btn btn-primary mt-2',
+        onClick: () => {
+          this.handleClick()
+        }
+      },
+      buttonText
+    )
+  }
+}
+
+const domContainer = document.getElementById('react_root')
+ReactDOM.render(
+  React.createElement(ClickButton),
+  domContainer
+)
