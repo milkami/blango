72688adb0128dfa294819908897e61f88e8ed5f1
Milka Miličević <milka.milicevic3@gmail.com>
2024-06-28 06:43:03 +0000

    Finish simplejwt

--- blango/settings.py
+++ blango/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 from configurations import Configuration
 from configurations import values
 import dj_database_url
+from datetime import timedelta
 
 class Dev(Configuration):
 
@@ -223,6 +224,7 @@ class Dev(Configuration):
         "rest_framework.authentication.BasicAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication"
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
@@ -245,6 +247,11 @@ class Dev(Configuration):
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.OrderingFilter"
     ],
+  }
+
+  SIMPLE_JWT = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
   }
 
 class Prod(Dev):
--- blog/api/urls.py
+++ blog/api/urls.py
@@ -8,6 +8,7 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 import os
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 router = DefaultRouter()
 router.register("tags", TagViewSet)
@@ -31,7 +32,9 @@ urlpatterns = [
 
 urlpatterns += [
     path("auth/", include("rest_framework.urls")),
-    path("token-auth/", views.obtain_auth_token),  
+    path("token-auth/", views.obtain_auth_token),
+    path("jwt/", TokenObtainPairView.as_view(), name="jwt_obtain_pair"),
+    path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt_refresh"),  
     re_path(
         r"^swagger(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(cache_timeout=0),
